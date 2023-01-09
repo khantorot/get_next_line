d3ecf9754c6881d5494a710bad0f803f525b4f84
khantorot <khantorot@gmail.com>
2023-01-10 04:34:09 +0600

    init

---
+++ README.md
@@ -0,0 +1,41 @@
+<br />
+<div align="center">
+  <a href="#top">
+    <img src="https://raw.githubusercontent.com/khantorot/intra42/master/content/icons/logo.png" alt="logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">get_next_line</h3>
+
+  <p align="center">
+    Function that can return a single line read from a file descriptor, without loosing track of the next line and with a random buffer size.
+    <br />
+    <br />
+    <a href="#top">view project +</a>
+  </p>
+</div>
+<br />
+
+
+
+
+
+### about
+
+Task file - [get_next_line.pdf](/get_next_line.pdf)
+
+
+
+<br />
+
+
+
+### contact
+
+Email - khantorot@gmail.com
+
+
+
+
+
+<br />
+<p align="center"><a href="#top">back to top ↑</a></p>
---
+++ get_next_line.c
@@ -0,0 +1,63 @@
+#include "get_next_line.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+char	*ft_read_to_left_str(int fd, char *left_str)
+{
+	char	*buff;
+	int		rd_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[rd_bytes] = '\0';
+		left_str = ft_strjoin(left_str, buff);
+	}
+	free(buff);
+	return (left_str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*left_str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	left_str = ft_read_to_left_str(fd, left_str);
+	if (!left_str)
+		return (NULL);
+	line = ft_get_line(left_str);
+	left_str = ft_new_left_str(left_str);
+	return (line);
+}
+
+int	main(void)
+{
+	char	*line;
+	int		i;
+	int		fd;
+
+	fd = open("test.txt", O_RDONLY);
+	i = 1;
+
+	while (i < 7)
+	{
+		line = get_next_line(fd);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	close(fd);
+	return (0);
+}
